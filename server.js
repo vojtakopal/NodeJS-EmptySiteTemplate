7ffe0487fe198d6462da57629d294d8d78f24d0b
Vojtech Kopal <vojtech.kopal@gmail.com>
2017-12-11 14:22:12 +0100

    return json

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hi, Vojta!');
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Hi, Vojta!' }));
     
 }).listen(process.env.PORT || 8080);
