1caebe6ea36c9e78aa386140cb0748cdf10b3979
Vojtech Kopal <vojtech.kopal@gmail.com>
2017-12-11 14:19:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hi, Vita!');
+    res.end('Hi, Vojta!');
     
 }).listen(process.env.PORT || 8080);
